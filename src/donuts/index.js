a99d7bacce3d363709302092c8f72e3ffe75ff31
Neto Russo <netoacrn1@gmail.com>
2024-03-01 11:29:25 -0300

    Subindo teste

---
+++ src/donuts/index.js
@@ -0,0 +1,17 @@
+import './donuts.css';
+import donutsSimbol from './Ativo 12.svg';
+import donutsSimbol2 from './Ativo 6.svg';
+import donutsSimbol3 from './Ativo 7.svg';
+import donutsSimbol4 from './comcalda.png';
+
+function Donuts() {
+    return(
+        <img 
+        className='simbolo'
+        src={donutsSimbol4}
+        alt='simbolo da donuts'
+        />
+     );
+};
+
+export default Donuts
