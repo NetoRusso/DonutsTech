1d54c5e0f837cae6135ca17eb5e9c6912a582cbf
Neto Russo <netoacrn1@gmail.com>
2024-03-02 02:13:36 -0300

    link

--- src/Banner/index.js
+++ src/Banner/index.js
@@ -14,7 +14,7 @@ function Banner() {
                         <span className='destaque2'> Tecnologia</span> que faz piruetas!<span><Donuts /></span></h2>
                     </div>          
                 </div>
-                    <button className='botao'> Conte com a gente</button> 
+                    <a className='botao' href='https://api.whatsapp.com/send?phone=5534999831153&text=Ol%C3%A1%20vim%20de%20seu%20site,%20e%20gostaria%20de%20saber%20mais' target='blank'> Conte com a gente </a> 
             </div>
         </div>
     )
