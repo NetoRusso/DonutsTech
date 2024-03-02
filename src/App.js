00f7eba7ec2d5eda8dd2154aff12ef9c24420ea3
Neto Russo <netoacrn1@gmail.com>
2024-03-02 19:51:29 -0300

    subido a producao

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './reset.css'
 import './App.css';
 import Banner from './Banner';
+import Sobre from './Sobre';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       <Banner />
+      <Sobre />
       </header>
     </div>
   );
---
+++ src/Sobre/index.js
@@ -0,0 +1,19 @@
+import './sobre.css'
+
+function Sobre() {
+    return (
+        <div className='conteudoTotal'>
+            <h4 className='titulo'>
+                Junte-se a nós nessa deliciosa jornada, onde a inovação e
+                a excelência se encontram para elevar o potencial do seu negócio!
+            </h4>
+            <p className='paragrafo'>
+                A Donuts Tech une tecnologia e design em uma jornada emocionante. Com expertise em artes visuais e software, destacamo-nos como uma força inovadora, integrando criatividade e tecnologia. Além de ser uma empresa de tecnologia, somos facilitadores de comunicação, criando layouts responsivos que maximizam o potencial do seu negócio.
+                Na Donuts Tech, misturamos inovação, cuidado artesanal e precisão técnica para criar obras-primas digitais que encantam e inspiram.
+            </p>
+        </div>
+
+    );
+}
+
+export default Sobre
