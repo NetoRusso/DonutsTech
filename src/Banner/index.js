f64d4fc532b57eb7e051ecbba3cf0f9516728472
Milena Leme <milenaleme4@hotmail.com>
2024-03-05 04:32:55 -0300

    modificando botao de link do botao conte com a gente

--- src/Banner/index.js
+++ src/Banner/index.js
@@ -1,29 +1,42 @@
-import './banner.css';
+import "./banner.css";
 //import logo from './logoDTH.svg';
-import Donuts from '../donuts';
-import logoClara from './LogoDtClara.svg';
+import Donuts from "../donuts";
+import logoClara from "./LogoDtClara.svg";
+import { Link } from "react-scroll";
 
 function Banner() {
-    return(
-        <div className='banner'>
-            <div className='containner' />
-            <div className='elementos'>
-
-            </div>
-            <div className='conteudo'>
-                <div>
-                <img className='logo' src={logoClara} alt='logo da donuts tech' />
-                    <div className='textos'>
-                        <h2 className='chamada'>O<span className='destaque1'> Marketing</span> que dá água na boca e <br/>a
-                        <span className='destaque2'> Tecnologia</span> que faz piruetas!<span><Donuts /></span></h2>
-                    </div>
-                </div>
-                    <a className='botao' href='https://api.whatsapp.com/send?phone=5534999831153&text=Ol%C3%A1%20vim%20de%20seu%20site,%20e%20gostaria%20de%20saber%20mais' target='blank'> Conte com a gente </a>
-            </div>
+  return (
+    <div className="banner">
+      <div className="containner" />
+      <div className="elementos"></div>
+      <div className="conteudo">
+        <div>
+          <img className="logo" src={logoClara} alt="logo da donuts tech" />
+          <div className="textos">
+            <h2 className="chamada">
+              O<span className="destaque1"> Marketing</span> que dá água na boca
+              e <br />a<span className="destaque2"> Tecnologia</span> que faz
+              piruetas!
+              <span>
+                <Donuts />
+              </span>
+            </h2>
+          </div>
         </div>
-    )
+        <Link
+          className="botao"
+          to="contatos"
+          smooth={true}
+          duration={500}
+          spy={true}
+          offset={-50}
+        >
+          {" "}
+          Conte com a gente{" "}
+        </Link>
+      </div>
+    </div>
+  );
+}
 
-};
-
-
-export default Banner
+export default Banner;
