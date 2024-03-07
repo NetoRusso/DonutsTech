c900e324386f6e7a3d40fb44a69134574eaa08fd
Neto Russo <netoacrn1@gmail.com>
2024-03-07 13:45:00 -0300

    retirando o form

--- src/Contatos/index.js
+++ src/Contatos/index.js
@@ -1,5 +1,5 @@
 import { Element } from 'react-scroll';
-import Formulario from './Formulario';
+// import Formulario from './Formulario';
 import style from './contatos.module.css';
 import Links from './Links';
 import detalhe from './malhaPositiva01.svg';
@@ -14,7 +14,7 @@ function Contatos() {
                 <h3>vamos tornar sua marca ainda mais irresistível para o mercado!</h3>
             </div>
             <div className={style.conteudo}>
-                <Formulario />
+                {/* <Formulario /> */}
                 <div className={style.ajuste}>
                 <Esmague />
                 <Links />
