7cffbcf850c96b40ce174916ca7ed2359ae023f6
pedroferreirasousa <pepeferreiras.pf@gmail.com>
2025-07-30 18:11:40 -0300

    subindo linktree da donuts

--- src/Linktree/Linktree.js
+++ src/Linktree/Linktree.js
@@ -1,11 +1,114 @@
-import './linktree.module.css';
+import "./linktree.css";
+
+//import fotos
+import logo from "./logoDTH.svg";
+import Neto from "./Neto.jpeg";
+import Milena from "./Milena.jpeg";
+import Pedro from "./Pedro.jpg";
+import Thiago from "./Thiago.jpg";
+import Whatsapp from "./whatsapp.png";
+import Instagram from "./insta.png";
+import Linkedin from "./linkedin.png";
+// import Computador from './computador.png';
+import Junio from "./Junio.jpg";
 
 const Linktree = () => {
-    return (
-        <div>
-            <h1>Linktree</h1>
+  return (
+    <div className="linktree-section">
+      <div className="container">
+        <div className="header">
+          <a href="https://www.donutstech.com.br/">
+            <img src={logo} alt="Logo" />
+          </a>
         </div>
-    )
+        <div className="titles">
+          <h2>Tecnologia com sabor.</h2>
+          <h3>Explore o universo Donuts Tech.</h3>
+        </div>
+        <div className="links">
+          <a href="#" className="link">
+            <div className="icon">
+              <img src={Whatsapp} width={28} height={28} />
+            </div>
+            <div className="texto">
+              <h3>Whatsapp</h3>
+            </div>
+          </a>
+
+          <a href="#" className="link">
+            <div className="icon">
+              <img src={Instagram} width={28} height={28} />
+            </div>
+            <div className="texto">
+              <h3>Instagram</h3>
+            </div>
+          </a>
+
+          <a href="#" className="link">
+            <div className="icon">
+              <img src={Linkedin} width={28} height={28} />
+            </div>
+            <div className="texto">
+              <h3>Linkedin</h3>
+            </div>
+          </a>
+          {/* <a href="#" className="link">
+            <div className="icon">
+              <img src={Computador} width={28} height={28} />
+            </div>
+            <div className="texto">
+              <h3>Portfólio</h3>
+            </div>
+          </a> */}
+        </div>
+        <div className="team">
+          <div className="profile">
+            <a href="#">
+              <img src={Neto} alt="Neto" width={52} height={52} />
+            </a>
+            <span>Neto</span>
+          </div>
+
+          <div className="profile">
+            <a href="#">
+              <img src={Milena} alt="Milena" width={56} height={56} />
+            </a>
+            <span>Milena</span>
+          </div>
+
+          <div className="profile">
+            <a href="#">
+              <img src={Pedro} alt="Pedro" width={56} height={56} />
+            </a>
+            <span>Pedro</span>
+          </div>
+        </div>
+        <div className="associados">
+          <h3>Associados</h3>
+          <div className="associados-list">
+              <a href="#">
+            <div className="associados-item">
+                <img src={Thiago} alt="" width={56} height={56} />
+                <span>
+                  Thiago <br />
+                  (Analista de Qualidade)
+                </span>
+            </div>
+              </a>
+            <a href="#">
+              <div className="associados-item">
+                <img src={Junio} alt="" width={56} height={56} />
+                <span>
+                  Júnio <br />
+                  (Analista de Infraestrutura)
+                </span>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Linktree;
