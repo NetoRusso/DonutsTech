00a2e98fa873601f915caa7159f9c1c3225c4347
pedroferreirasousa <pepeferreiras.pf@gmail.com>
2025-07-30 15:11:22 -0300

    feat: iniciando projeto, e criando rota para linktree

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import './App.css';
-import Banner from './Banner';
-import Contatos from './Contatos';
-import Menu from './Menu';
-import Sobre from './Sobre';
-import Solucoes from './Solucoes';
+import "./App.css";
+import Banner from "./Banner";
+import Contatos from "./Contatos";
+import Menu from "./Menu";
+import Sobre from "./Sobre";
+import Solucoes from "./Solucoes";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Linktree from "./Linktree/Linktree";
 
 function App() {
-  console.log('Olá amigo DeV. que bom te ver por aqui, gostou do que viu? Ficou com alguma duvida? Qualquer coisa entre em contato que podemos te oferecer uma consultoria🍩');
+  console.log(
+    "Olá amigo DeV. que bom te ver por aqui, gostou do que viu? Ficou com alguma duvida? Qualquer coisa entre em contato que podemos te oferecer uma consultoria🍩"
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-      <Banner />
-      <Menu />
-      <Sobre />
-      <Solucoes />
-      <Contatos />
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="App">
+              <header className="App-header">
+                <Banner />
+                <Menu />
+                <Sobre />
+                <Solucoes />
+                <Contatos />
+              </header>
+            </div>
+          }
+        />
+
+        <Route path="/linktree" element={<Linktree />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Linktree/Linktree.js
@@ -0,0 +1,11 @@
+import './linktree.module.css';
+
+const Linktree = () => {
+    return (
+        <div>
+            <h1>Linktree</h1>
+        </div>
+    )
+};
+
+export default Linktree;
